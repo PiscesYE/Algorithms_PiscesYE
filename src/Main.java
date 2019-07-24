77ec89adc82b17125f68e0206b21b3154ec0faea
PiscesYE <2690057415>
2019-07-24 09:23:27 +0800

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import base.LinkedList;
 import base.SequentialList;
 
 import java.util.ArrayList;
@@ -8,6 +9,15 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        HashMap<String, String> test = new HashMap<>();
+        LinkedList<String> test = new LinkedList<>();
+        test.put("第一个节点");
+        test.put("球球");
+        test.put("第一个节点","插入的节点");
+        System.out.println(test.toString());
+        System.out.println("当前节点数："+test.size());
+        test.remove("插入的节点");
+        test.remove("第一个节点");
+        System.out.println(test.toString());
+        System.out.println("当前节点数："+test.size());
     }
 }
--- src/base/LinkedList.java
+++ src/base/LinkedList.java
@@ -23,6 +23,8 @@ package base;
  * 劣势：数据访问时只能顺着链表查找，自由度和效率都存在一定的缺陷
  * <p>
  * Note：非线程安全
+ * <p>
+ * 设计上的问题：到底要不要存储重复的值，暂时允许存储重复的值
  *
  * @author PiscesYE
  */
@@ -64,6 +66,13 @@ public class LinkedList<T extends Object> {
             node = data;
         }
 
+        @Override
+        public boolean equals(Object obj) {
+            if (node.equals(node)) {
+                return true;
+            }
+            return false;
+        }
     }
 
     //增
@@ -98,6 +107,39 @@ public class LinkedList<T extends Object> {
     }
 
     /**
+     * 在指定的节点后添加新节点
+     *
+     * @param node 在该节点后添加
+     * @param add  添加的新节点
+     * @return
+     */
+    public boolean put(T node, T add) {
+        if (node == null || add == null) {
+            System.out.println("节点数据不能为空");
+            return false;
+        }
+
+        if (size == 0) {
+            System.out.println("当前链表为空");
+            return false;
+        }
+
+        Node jump = root;
+        for (int i = 1; i < size; i++) {
+            if (jump.node == node && jump.node.equals(node)) {
+                Node addNode = new Node(add);
+                addNode.next = jump.next;
+                jump.next = addNode;
+                size++;
+                return true;
+            }
+            jump = jump.next;
+        }
+        System.out.println("当前链表中未找到该节点");
+        return false;
+    }
+
+    /**
      * 插入头节点
      *
      * @param data 插入节点数据
@@ -112,44 +154,124 @@ public class LinkedList<T extends Object> {
         Node add = new Node(data);
         if (size == 0) {
             root = add;
+            size++;
             return true;
         } else {
             add.next = root;
             root = add;
+            size++;
             return true;
         }
     }
 
     //删
 
+    /**
+     * 移除指定节点数据的节点
+     * 暂时允许存储重复的值，所以需要删除的节点可能不止一个
+     *
+     * 有问题，不能删除第一个节点
+     *
+     * @param data 需要删除包含该数据的节点
+     * @return 删除结果
+     */
+    public boolean remove(T data) {
+        if (data == null) {
+            System.out.println("节点数据不能为空");
+            return false;
+        }
+
+        if (size == 0) {
+            System.out.println("当前链表为空");
+            return false;
+        }
+
+        //TODO 有问题，不能删除第一个节点
+        Node jump = root;
+        Node parent = jump;
+        boolean result = false;
+        for (int i = 1; i < size; i++) {
+            if (jump.node == data && jump.node.equals(data)) {
+                if (--size == 0) {
+                    root = null;
+                } else {
+                    parent.next = jump.next;
+                }
+                result = true;
+            }
+            parent = jump;
+            jump = jump.next;
+        }
+        if (!result){
+            System.out.println("当前链表未找到该节点");
+        }
+        return result;
+    }
+
     //改
 
     //查
 
     /**
+     * 链表是否为空
+     *
+     * @return true 空
+     */
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取链表的长度
+     * @return The number of linked list
+     */
+    public int size(){
+        return size;
+    }
+
+    /**
      * 查询节点数据
      * 根据参数查找，但是我目前的节点没有查找的条件
+     *
      * @param data
      * @return
      */
-    public int get(T data){
-        if (data == null){
+    public int get(T data) {
+        if (data == null) {
             System.out.println("节点数据不能为空");
             return 0;
         }
 
-        if (size == 0){
+        if (size == 0) {
             System.out.println("当前链表为空");
             return 0;
-        }else{
+        } else {
             Node end = root;
             for (int i = 1; i < size; i++) {
-                if (end.node == data){
+                if (end.node == data && end.node.equals(data)) {
                     return i;
                 }
             }
             System.out.println("链表中没有该节点数据");
             return 0;
         }
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        if (size > 0){
+            Node jump = root;
+            for (int i = 0; i < size; i++) {
+                sb.append(jump.node.toString()+" -> ");
+                jump = jump.next;
+            }
+        }
+        sb.append("null");
+        return sb.toString();
     }
 }
