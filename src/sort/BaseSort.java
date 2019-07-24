7b10aac7205d958c14f8ebd513707f1bde782e54
PiscesYE <2690057415>
2019-07-24 17:33:08 +0800

    sort

---
+++ src/sort/BaseSort.java
@@ -0,0 +1,18 @@
+package sort;
+
+/**
+ * 基本排序算法
+ * <p>
+ * 基本排序算法对整形数据进行排序
+ *
+ * @author PiscesYE
+ */
+public class BaseSort {
+
+    /**
+     * 冒泡排序
+     */
+    public static void bubbleSort(int array[]) {
+
+    }
+}
