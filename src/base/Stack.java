3ca974be1bdd9b15a20f15fe8d5416833490dff0
PiscesYE <2690057415>
2019-07-24 14:06:45 +0800

    ok

---
+++ src/base/Stack.java
@@ -0,0 +1,37 @@
+package base;
+
+/**
+ * Stack - 栈
+ * 对数据的现场保护有重要意义
+ *
+ * 基本核心为：
+ * 使用连续的内存或以链表的形式存储元素的值，初始的一端定义为栈底
+ * 用于操作的一端称之为栈顶，只能从栈顶操作栈
+ *
+ * 操作原则：LIFO，last in first out
+ *
+ * 基本结构：
+ *
+ *
+ * <p>
+ * 分类：
+ * 顺序栈
+ * 链式栈
+ *
+ * 优势：
+ * 劣势：
+ *
+ * @author PiscesYE
+ */
+public class Stack {
+
+    //入栈
+    public boolean push(){
+        return true;
+    }
+
+    //出栈
+    public boolean pop(){
+        return true;
+    }
+}
