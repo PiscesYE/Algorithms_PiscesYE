11b45b7c12dadd5f64db4117e9d5b38355e84d7b
PiscesYE <2690057415>
2019-07-24 17:46:45 +0800

    sort

--- src/sort/BaseSort.java
+++ src/sort/BaseSort.java
@@ -11,8 +11,24 @@ public class BaseSort {
 
     /**
      * 冒泡排序
+     * 方式：大的往后放或者小的往前调（排序方向）
+     * 当前：大的往后放
      */
     public static void bubbleSort(int array[]) {
+        if (array == null) {
+            System.out.println("待排序数组不可为空");
+            return;
+        }
+        int size = array.length;
+        if (size == 1) {
+            return;
+        }
 
+        //排序
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+
+            }
+        }
     }
 }
