865bcfbf8e474ade4540bd36060925c622a79e85
PiscesYE <2690057415>
2019-07-26 13:18:02 +0800

    sort

---
+++ src/leetcode/regex/RegularExpression.java
@@ -0,0 +1,30 @@
+package leetcode.regex;
+
+/**
+ * Regular Expression - 正则表达式
+ *
+ * @author PiscesYE
+ */
+public class RegularExpression {
+
+    /**
+     * Regular Expression Matching - 正则表达式匹配
+     *
+     * given an input string(s) and a pattern(p), implement
+     * regular expression matching with support for '.' and '*'
+     *
+     * sign:
+     * '.' matches any single character
+     * '*' matches zero or more of preceding element
+     *
+     * Note: the matching should cover the entire input string (not partial)
+     *
+     * @param s could be empty and contains only lowercase letters a-z
+     * @param p could be empty and contains only lowercase letters a-z, and character .or *
+     * @return true if matching | otherwise false
+     */
+    public static boolean RegexMatching(String s, String p){
+
+        return true;
+    }
+}
